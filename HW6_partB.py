002795f8abfee544a0a81624d3bd5568cc1d1ee8
Keely Meyers <keelym@umich.edu>
2017-10-04 20:18:14 -0400

    looping through all tags

--- HW6_partB.py
+++ HW6_partB.py
@@ -2,9 +2,8 @@ import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import re
 
-url = "http://py4e-data.dr-chuck.net/known_by_Ka.html"
+url = "http://py4e-data.dr-chuck.net/known_by_Fikret.html"
 html = urllib.request.urlopen(url).read()
 soup = BeautifulSoup(html, 'html.parser')
-tags = soup()
-for tag in tags:
-	
+tags = soup('a')
+print (tags)
