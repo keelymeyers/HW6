72d0bf2de5179d1da08854bcca9b69e0be1c3dc8
Keely Meyers <keelym@umich.edu>
2017-10-06 21:11:14 -0400

    Finished HW 6

--- HW6_partB.py
+++ HW6_partB.py
@@ -2,10 +2,22 @@ import urllib.request, urllib.parse, urllib.error
 from bs4 import BeautifulSoup
 import re
 
-url = "http://py4e-data.dr-chuck.net/known_by_Fikret.html"
-html = urllib.request.urlopen(url).read()
-soup = BeautifulSoup(html, 'html.parser')
-tags = soup('a')
-for tag in tags:
-	print (tag.get('href', None))
+url = "http://py4e-data.dr-chuck.net/known_by_Ka.html"
+
+count = 7
+h_list = []
+while count != 0:
+	html = urllib.request.urlopen(url).read()
+	soup = BeautifulSoup(html, 'html.parser')
+	tags = soup('a')
+	url = tags[17].get('href', None)
+	h_list.append(url)
+	html = urllib.request.urlopen(url).read()
+	count = count - 1
+print (h_list[-1])
+
+
 	
+
+
+
